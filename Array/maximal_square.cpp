ae082a80188a916f3413f0d4335398b12731143c
Shreyash Jadhav <shreyashjadhav1002@gmail.com>
2022-11-01 09:07:04 +0530

    added maximal square question

---
+++ Array/maximal_square.cpp
@@ -0,0 +1,29 @@
+/*
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+*/
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0, pre;
+        vector<int> cur(n, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = cur[j];
+                if (!i || !j || matrix[i][j] == '0') {
+                    cur[j] = matrix[i][j] - '0';
+                } else {
+                    cur[j] = min(pre, min(cur[j], cur[j - 1])) + 1;
+                }
+                sz = max(cur[j], sz);
+                pre = temp;
+            }
+        }
+        return sz * sz;
+    }
+};
